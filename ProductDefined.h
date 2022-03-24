4b378215969a14e46719722478d18b22bf7b9959
user <user@ubuntu.ubuntu-domain>
2022-03-24 11:36:29 +0300

    finsish demo program

--- ProductDefined.h
+++ ProductDefined.h
@@ -1,24 +1,43 @@
+#include <iostream>
 #include <string.h>
 #include "product.h"
 
 Product::Product() {
+    this->name = new char[strlen("")];
+    strcpy(this->name, "");
+    
+    this->code = new char[strlen("")];
+    strcpy(this->code, "");
+    
     this->quantity = 1;
+
+    std::cout << this->code << " Product::Product()" << "\n";
 }
 Product::Product(char *name, char *code, int quantity=1) {
     this->name = new char[strlen(name)];
     strcpy(this->name, name);
+    
     this->code = new char[strlen(code)];
     strcpy(this->code, code);
+    
     this->quantity = quantity;
+
+    std::cout << this->code << " Product::Product(char *name, char *code, int quantity=1)" << "\n";
 }
 Product::Product(const Product& pr) {
     this->name = new char[strlen(pr.name)];
     strcpy(this->name, pr.name);
+    
     this->code = new char[strlen(pr.code)];
     strcpy(this->code, pr.code);
+    
     this->quantity = pr.quantity;
+
+    std::cout << this->code << " Product::Product(const Product& pr)" << "\n";
 }
 Product::~Product() {
+    std::cout << this->code << " Product::~Product()" << "\n";
+    
     delete this->name;
     delete this->code;
 }
--- main.cpp
+++ main.cpp
@@ -4,11 +4,24 @@
 using namespace std;
 
 int main() {
-    char name[] = "Alex";
-    char code[] = "123124";
-    Product *pr = new Product(name, code, 5);
+    Product *pr1 = new Product("Gear", "id_1", 10);
+    Product *pr2 = new Product();
 
-    cout << pr->getName() << ' ' << pr->getCode() << ' ' << pr->getQuantity() << endl;
+    cout << pr1->getName() << ' ' << pr1->getCode() << ' ' << pr1->getQuantity() << endl;
+    cout << pr2->getName() << ' ' << pr2->getCode() << ' ' << pr2->getQuantity() << endl;
+
+    pr2 = new Product(*pr1);
+    pr2->setCode("id_2");
+
+    cout << pr1->getName() << ' ' << pr1->getCode() << ' ' << pr1->getQuantity() << endl;
+    cout << pr2->getName() << ' ' << pr2->getCode() << ' ' << pr2->getQuantity() << endl;
+    
+    int (Product::*pointer)();
+    pointer = &Product::getQuantity;
+    cout << (pr1->*pointer)() << endl;
+    
+    delete pr1;
+    delete pr2;
 
     return 0;
 }
