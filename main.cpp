2753a8dad4d45e525c9853e5f07aaa03da7d16d5
user <user@ubuntu.ubuntu-domain>
2022-03-24 11:51:15 +0300

    added comments

--- main.cpp
+++ main.cpp
@@ -4,24 +4,29 @@
 using namespace std;
 
 int main() {
-    Product *pr1 = new Product("Gear", "id_1", 10);
-    Product *pr2 = new Product();
+    //указатель на объект
+    Product *pr1 = new Product("Gear", "id_1", 10); //вызов конструктора с параметрами
+        //Product * - тип указателя
+        //pr1 - переменная
+        //new Product() - вызывает конструктор
+    Product *pr2 = new Product(); //вызов конструктора без параметрами
 
     cout << pr1->getName() << ' ' << pr1->getCode() << ' ' << pr1->getQuantity() << endl;
     cout << pr2->getName() << ' ' << pr2->getCode() << ' ' << pr2->getQuantity() << endl;
 
-    pr2 = new Product(*pr1);
+    pr2 = new Product(*pr1); //вызов конструктора копирования
     pr2->setCode("id_2");
 
     cout << pr1->getName() << ' ' << pr1->getCode() << ' ' << pr1->getQuantity() << endl;
     cout << pr2->getName() << ' ' << pr2->getCode() << ' ' << pr2->getQuantity() << endl;
     
+    //pointer - указатель на метод Product::getQuantity
     int (Product::*pointer)();
     pointer = &Product::getQuantity;
     cout << (pr1->*pointer)() << endl;
     
-    delete pr1;
-    delete pr2;
+    delete pr1; //вызов деструктора
+    delete pr2; //вызов деструктора
 
     return 0;
 }
--- product.h
+++ product.h
@@ -1,28 +1,41 @@
 #ifndef PRODUCTH
 #define PRODUCTN
 
+//класс Изделие
 class Product {
 
     private:
     
+    //Имя
     char *name;
+    //Код
     char *code;
+    //Количество
     int quantity;
 
     public:
 
+//Конструкторы
+    //без параметров
     Product();
+    //с параметрами
     Product(char *name, char *code, int quantity);
+    //копирования
     Product(const Product&);
 
+//Деконструктор
     ~Product();
 
+//сеттеры и геттеры
+    //для имени
     void setName(char *name);
     char *getName();
 
+    //для кода
     void setCode(char *code);
     char *getCode();
         
+    //для количества
     void setQuantity(int qt);
     int getQuantity();
 };
